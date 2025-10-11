7c393a8cc9a3a3923931fd5691c5bae6dd9c94cb
Kalidasan S <kalidasansmail002@gmail.com>
2025-10-11 21:01:15 +0530

    first commit

---
+++ hwork1.sql
@@ -0,0 +1,6 @@
+SELECT * FROM books WHERE price > 400;
+SELECT * FROM books WHERE genre IN ('History', 'Science', 'Fiction');
+SELECT * FROM books WHERE title = 'The Great Gatsby';
+SELECT * FROM books WHERE author <> 'Dan Brown';
+
+
